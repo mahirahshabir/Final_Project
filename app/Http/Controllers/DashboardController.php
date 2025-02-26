5773c453057010e3affc1629fbe843d70bebeb62
Mahirah Shabir <153275015+mahirahshabir@users.noreply.github.com>
2025-02-26 15:31:16 +0530

    change in dashboard and models phase,task and change in controllers intall alpine js

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,15 +5,18 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Project;
 use App\Models\Task;
+use App\Models\Phase;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-       $users = User::all(); // Fetch all users
+    $phases = Phase::with('tasks')->get();
+    $users = User::all(); // Fetch all users
     $projects = Project::all(); // Fetch all projects
     $tasks = Task::all(); // Fetch all tasks
 
-    return view('dashboard', compact('users', 'projects', 'tasks'));
+    return view('dashboard', compact('phases','users', 'projects', 'tasks'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 // Dashboard Route
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+Route::post('/update-task-phase', [TaskController::class, 'updateTaskPhase'])->name('update-task-phase');
+
 // Protected Routes for Authenticated Users
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout'); // ✅ Moved inside the same group
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout'); //  Moved inside the same group
 });
